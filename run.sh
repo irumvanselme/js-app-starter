2e65ce8adb1d5e9eba0618320451214ac61fad92
Anselme Habumugisha Irumva <andesanselme@gmail.com>
2023-03-10 11:53:27 +0200

    feat: initial connections

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+eval "$(
+  cat .env | awk '!/^\s*#/' | awk '!/^\s*$/' | while IFS='' read -r line; do
+    key=$(echo "$line" | cut -d '=' -f 1)
+    value=$(echo "$line" | cut -d '=' -f 2-)
+    echo "export $key=\"$value\""
+  done
+)"
+
+echo $MESSAGE
